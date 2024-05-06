7ba361545714ddece85d080704917bf79e6ccb7e
leno1moraes <lenomoraes12@gmail.com>
2024-05-06 03:05:31 -0400

    primeiro commit

---
+++ VM-host-100/demo/serverapache.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VM 100</title>
+
+    <link href="bootstrap/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <div class="container">
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                
+              <a class="navbar-brand" href="/">Home</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                <ul class="navbar-nav">
+                  <li class="nav-item dropdown">
+                    <button class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                      Inicio
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-primary">
+                      <li><a class="dropdown-item" href="meuhosts.php">Meu Host</a></li>
+                      <li><a class="dropdown-item" href="serverapache.php">Servidor Apache</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>              
+            </div>
+        </nav>       
+                
+        <main>
+            <h1>Servidor Apache</h1>
+        </main>
+
+    </div>
+    
+    <script src="bootstrap/bootstrap.bundle.min.js"></script>
+</body>
+</html>
